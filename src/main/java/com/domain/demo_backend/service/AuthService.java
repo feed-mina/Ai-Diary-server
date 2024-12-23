0d71caa865fcecafecc2954763061a8066f7ca10
feed-mina <myelin24@naver.com>
2024-12-24 00:37:13 +0900

    회원 가입 유효성 검증중 2024-12-24

--- src/main/java/com/domain/demo_backend/service/AuthService.java
+++ src/main/java/com/domain/demo_backend/service/AuthService.java
@@ -31,12 +31,11 @@ public class AuthService {
         System.out.println("입력된 해시값: " + PasswordUtil.sha256(loginRequest.getPassword()));
 
         if (user == null) {
-            System.out.println("아이디 성공");
+            System.out.println("아이디 실패");
             throw new RuntimeException("존재하지 않는 아이디입니다.");
         }
         if (!user.getHashedPassword().equals(PasswordUtil.sha256(loginRequest.getPassword()))) {
-
-            System.out.println("비밀번호 성공");
+            System.out.println("비밀번호 실패");
             throw new RuntimeException("비밀번호가 일치하지 않습니다.");
         }
         System.out.println("JWT 성공");
